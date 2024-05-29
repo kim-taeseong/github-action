2d100cc0d46e9ec0bef8b81776ae2ab15712327b
taeseong <dnfrkwhr2424@naver.com>
2024-05-29 13:49:03 +0900

    feat: allowed host 추가

--- project_config/settings.py
+++ project_config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@z%sk(+=n7y-b-pzlg@xj@^o0-w_v64rp247nt$rem@i2u-3-c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
